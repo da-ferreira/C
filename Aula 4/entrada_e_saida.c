f9a6216a55a684b95b028478a2dc9cbeee302936
da-ferreira <ferreiradavid361@gmail.com>
2022-03-11 20:13:19 -0300

    primeiro commit

---
+++ Aula 4/entrada_e_saida.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ 
+ int main() {
+	int idade = 20, dia = 10, mes = 5, ano = 2000;
+	unsigned long long int numero_grande = 100200300400500;
+	
+	printf("Hello World!\n");
+	printf("Sua idade eh %d\n", idade);
+	printf("%llu eh um numero muito grande\n", numero_grande);
+	printf("Maria nasceu em %d/%d/%d\n", dia, mes, ano);
+	
+ 	return 0;
+ }
+ 
---
+++ Aula 5/exemplo_trava_tela.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+#include<conio.h>  // Só funciona no Windows
+
+int main() {
+	printf("Ola, mundo\n");
+	//getch();  // Trava o programa pro usuário
+	system("pause");  // Não precisa de importação. É o pause do Windows
+	return 0;
+	
+}
