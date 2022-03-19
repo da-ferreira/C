517db5d2261cb013e8dc851c07d3a7239de5d33c
da-ferreira <ferreiradavid361@gmail.com>
2022-03-18 22:37:52 -0300

    Aula 6 | modifica strings.c

--- Aula 6/strings.c
+++ Aula 6/strings.c
@@ -3,22 +3,22 @@
  #include<string.h>  // Varias funcoes para manipular Strings
  
  int main() {
- 	char mensagem[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
- 	printf("Variavel 'mensagem': %s\n", mensagem);
- 	
-  	char outra_mensagem[] = "Hello";  // O \0 é adiciona automaticamente
-  	printf("Variavel 'outra_mensagem': %s\n", outra_mensagem);
-  	
-    printf("Tamanho em bytes de 'outra_mensagem': %ld\n", sizeof(outra_mensagem));
- 	printf("Tamanho em bytes de 'mensagem': %ld\n", sizeof(mensagem));
- 	
+	char mensagem[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+	printf("Variavel 'mensagem': %s\n", mensagem);
+	
+	char outra_mensagem[] = "Hello";  // O \0 é adiciona automaticamente
+	printf("Variavel 'outra_mensagem': %s\n", outra_mensagem);
+	
+	printf("Tamanho em bytes de 'outra_mensagem': %ld\n", sizeof(outra_mensagem));
+	printf("Tamanho em bytes de 'mensagem': %ld\n", sizeof(mensagem));
+	
 	char cidade[20];
 	printf("Informe a cidade: ");
 	scanf("%19[0-9a-zA-Z ]s", cidade);  // Especificando o tamanho maximo aceito pela string cidade (char). Usando expressão regular também
- 	printf("Cidade digitada: %s\n", cidade);
- 	
- 	printf("Tamanho da string 'cidade': %d\n", strlen(cidade));
- 	
- 	return 0;
+	printf("Cidade digitada: %s\n", cidade);
+	
+	printf("Tamanho da string 'cidade': %d\n", strlen(cidade));
+	
+	return 0;
  }
  
